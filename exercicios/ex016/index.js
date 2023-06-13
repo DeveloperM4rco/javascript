e9063628b6164ca6bbaba4d61d7db0d0f4fbda87
Marco Antônio <marcoantoniocamposcarrocon@gmail.com>
2023-06-13 14:18:55 -0300

    repetições com testes lógicos

--- exercicios/ex016/index.js
+++ exercicios/ex016/index.js
@@ -1,6 +1,12 @@
 var i = 1
-// equanto a condição for verdadeira o bloco será executado
+// o teste lógico é feito no início
 while (i <= 6) {
     console.log(`Contador no valor ${i}`);
     i++
 }
+
+do {
+    console.log(`Contador no valor ${i}`);
+    i++
+    // o teste lógico é feito no final
+} while (i <= 6);
