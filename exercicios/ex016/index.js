e929cd42e4e6b5307bf2064ac7229d5c4d625542
Marco Antônio <marcoantoniocamposcarrocon@gmail.com>
2023-06-13 14:33:56 -0300

    repetições com teste lógico

--- exercicios/ex016/index.js
+++ exercicios/ex016/index.js
@@ -2,11 +2,11 @@ var i = 1;
 // o teste lógico é feito no início
 while (i <= 6) {
     console.log(`Contador no valor ${i}`);
-    i++
+    i++;
 }
 
 do {
     console.log(`Contador no valor ${i}`);
-    i++
+    i++;
     // o teste lógico é feito no final
 } while (i <= 6);
