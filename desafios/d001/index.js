aea4a2627d7b8adb2bd1b4258cb3ccfad74e16f8
Marco Antônio <marcoantoniocamposcarrocon@gmail.com>
2023-07-02 19:01:23 -0300

    desafio 1 de JS

---
+++ desafios/d001/index.js
@@ -0,0 +1,13 @@
+let botao1 = document.querySelector("#botao1"), botao2 = document.querySelector("#botao2"), botao3 = document.querySelector("#botao3");
+
+botao1.addEventListener('click', function() {
+    alert('Você clicou no 1º botão');
+});
+
+botao2.addEventListener('click', function() {
+    alert('Você clicou no 2º botão');
+});
+
+botao3.addEventListener('click', function() {
+    alert('Você clicou no 3º botão')
+});
