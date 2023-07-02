9f349209f0eea82eb3c5257020a886e3cc4eb3fe
Marco Antônio <marcoantoniocamposcarrocon@gmail.com>
2023-07-02 19:13:05 -0300

    desafio de boas vindas

---
+++ desafios/d002/index.js
@@ -0,0 +1,9 @@
+let botao = document.querySelector(".botao");
+
+botao.addEventListener('click', clicou);
+
+function clicou() {
+    let nome = prompt('Qual é o seu nome?');
+    let idade = prompt(`Olá, ${nome}! Quantos anos você tem?`)
+    alert(`Acabei de conhecer ${nome}, que tem ${idade} anos de idade!`)
+}
